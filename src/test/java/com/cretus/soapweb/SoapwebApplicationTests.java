f9c2aeabb237871b8855ec2476bc88d203ed7a7b
cretusmukiza <cretusmukiza@gmail.com>
2020-10-10 20:38:08 +0300

    soap webservices

---
+++ src/test/java/com/cretus/soapweb/SoapwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cretus.soapweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
